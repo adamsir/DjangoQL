2f77da22a1df38496ce7191470c35f49ce6f25de
adamsir <donuteeze@gmail.com>
2019-03-13 04:01:14 +0100

    Update apps.py

--- eshop/product/apps.py
+++ eshop/product/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ProductConfig(AppConfig):
     name = 'eshop.product'
     label = 'products'
-    verbose_name = 'Products'
+    verbose_name = 'Application'
